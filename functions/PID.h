ecbbecac609672f08f63a24824e0018a5242499f
Vincent Liu <vincent.liu@DRX.ACADEMY>
2019-01-26 16:26:16 -0500

    blah

--- functions/PID.h
+++ functions/PID.h
@@ -3,9 +3,3 @@ typedef struct{
 	double *output, *input;
 	double integrator, previousError;
 }PID_Values;
-
-<<<<<<< HEAD
-//hello
-=======
-//gabe was here
->>>>>>> 4540e1d3fee59f282138ae092e6550471daf1cda
