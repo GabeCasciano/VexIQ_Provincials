72ecbab577e3e38597d190837731f094339f787a
gabecasciano <gabecasciano@gmail.com>
2019-01-26 16:27:39 -0500

    Stuf i gess

--- functions/PID.h
+++ functions/PID.h
@@ -1,11 +1,6 @@
+
 typedef struct{
 	double Kp, Ki, Kd;
 	double *output, *input;
 	double integrator, previousError;
 }PID_Values;
-
-<<<<<<< HEAD
-//hello
-=======
-//gabe was here
->>>>>>> 4540e1d3fee59f282138ae092e6550471daf1cda
