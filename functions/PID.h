f099807f8f0a08b740699c92ba787e384bfb1737
gabecasciano <gabecasciano@gmail.com>
2019-01-26 16:37:45 -0500

    Finished PID

--- functions/PID.h
+++ functions/PID.h
@@ -1,6 +1,23 @@
 
 typedef struct{
+	int target, deadband;
 	double Kp, Ki, Kd;
 	double *output, *input;
 	double integrator, previousError;
 }PID_Values;
+
+bool calculatePID(PID_Values value){
+	int error = value->target - values->*input;
+	double proportional = Kp * error;
+	double derivatie = Kd * (error - value->previousError);
+	value->previousError = error;
+	value->integrator += Ki * error;
+	if(abs(error) > deadband)
+		value->*output = proportional + derivative + 
+value->integrator;
+	else{
+		value->*output = 0;
+		return true;
+	}
+	return false;
+}
